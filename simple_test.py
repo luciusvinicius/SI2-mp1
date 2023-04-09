5d9ccc50fca45a328f7ce1f14459d7f84195c7be
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-04-09 20:59:11 +0100

    add pytest fixture

--- simple_test.py
+++ simple_test.py
@@ -1,14 +1,27 @@
-
+import pytest
 from typing import Dict, List
 from sn.kb import EntityType, KnowledgeBase, RelType, Relation
 from sn.confidence import ConfidenceTable
 from copy import copy
 
-kb = KnowledgeBase("bolt://localhost:7687", "neo4j", "Sussy_baka123321")
+@pytest.fixture(autouse=True)
+def initialize_knowledge_base():
+    kb = KnowledgeBase("bolt://localhost:7687", "neo4j", "Sussy_baka123321")
+    kb.delete_all()
+    
+    yield kb
+    
+    kb.delete_all()
+    kb.close()
 
-def add_data1():
+@pytest.fixture()
+def data1(initialize_knowledge_base):
+
+    kb = initialize_knowledge_base
+
     kb.add_knowledge("Lucius", Relation("Diogo", EntityType.INSTANCE, "cringe", EntityType.TYPE, "is", RelType.OTHER))
     kb.add_knowledge("Lucius", Relation("Diogo", EntityType.INSTANCE, "person", EntityType.TYPE, "is" , RelType.INHERITS))
+    kb.add_knowledge("Lucius", Relation("Lucius", EntityType.INSTANCE, "person", EntityType.TYPE, "is" , RelType.INHERITS))
     kb.add_knowledge("Lucius", Relation("Diogo", EntityType.INSTANCE, "working", EntityType.TYPE, "is", RelType.OTHER))
     kb.add_knowledge("Diogo", Relation("Lucius", EntityType.INSTANCE, "bad declarator", EntityType.TYPE, "is", RelType.OTHER))
     kb.add_knowledge("Diogo", Relation("Lucius", EntityType.INSTANCE, "mushrooms", EntityType.TYPE, "likes", RelType.OTHER))
@@ -17,18 +30,39 @@ def add_data1():
     kb.add_knowledge("Lucius", Relation("mammal", EntityType.TYPE, "animal", EntityType.TYPE, "is", RelType.INHERITS))
     kb.add_knowledge("Martinho", Relation("Diogo", EntityType.INSTANCE, "cringe", EntityType.TYPE, "is", RelType.OTHER, not_=True))
     
-
     kb.add_knowledge("Diogo", Relation("person", EntityType.TYPE, "food", EntityType.TYPE, "eats", RelType.OTHER))
     kb.add_knowledge("Diogo", Relation("person", EntityType.TYPE, "beans", EntityType.TYPE, "eats", RelType.OTHER))
     kb.add_knowledge("Diogo", Relation("Diogo", EntityType.INSTANCE, "chips", EntityType.TYPE, "eats", RelType.OTHER))
     kb.add_knowledge("Lucius", Relation("mammal", EntityType.TYPE, "banana", EntityType.TYPE, "eats", RelType.OTHER))
     kb.add_knowledge("Lucius", Relation("animal", EntityType.TYPE, "water", EntityType.TYPE, "drinks", RelType.OTHER))
+    
+    return kb
 
+@pytest.fixture()
+def data2(initialize_knowledge_base):
 
-add_data1()
+    kb = initialize_knowledge_base
 
+    kb.add_knowledge("Lucius", Relation("Diogo", EntityType.INSTANCE, "cringe", EntityType.TYPE, "is", RelType.OTHER))
+    kb.add_knowledge("Lucius", Relation("Diogo", EntityType.INSTANCE, "person", EntityType.TYPE, "is" , RelType.INHERITS))
+    kb.add_knowledge("Lucius", Relation("Lucius", EntityType.INSTANCE, "person", EntityType.TYPE, "is" , RelType.INHERITS))
+    kb.add_knowledge("Lucius", Relation("Diogo", EntityType.INSTANCE, "working", EntityType.TYPE, "is", RelType.OTHER))
+    kb.add_knowledge("Diogo", Relation("Lucius", EntityType.INSTANCE, "bad declarator", EntityType.TYPE, "is", RelType.OTHER))
+    kb.add_knowledge("Diogo", Relation("Lucius", EntityType.INSTANCE, "mushrooms", EntityType.TYPE, "likes", RelType.OTHER))
+    kb.add_knowledge("Diogo", Relation("Lucius", EntityType.INSTANCE, "shotos", EntityType.TYPE, "likes", RelType.OTHER))
+    kb.add_knowledge("Martinho", Relation("person", EntityType.TYPE, "mammal", EntityType.TYPE, "is", RelType.INHERITS))
+    kb.add_knowledge("Lucius", Relation("mammal", EntityType.TYPE, "animal", EntityType.TYPE, "is", RelType.INHERITS))
+    kb.add_knowledge("Martinho", Relation("Diogo", EntityType.INSTANCE, "cringe", EntityType.TYPE, "is", RelType.OTHER, not_=True))
+    
+    kb.add_knowledge("Diogo", Relation("Diogo", EntityType.INSTANCE, "chips", EntityType.TYPE, "eats", RelType.OTHER))
+    
+    return kb
+    
 
-def test_diogo_eats():
+def test_diogo_eats(data1):
+    
+    kb = data1
+    
     output = {
         "Diogo": (["chips"], 0),
         "person": (["food", "beans"], 1), # TODO: Food order is random. Maybe order in the query?
@@ -36,12 +70,14 @@ def test_diogo_eats():
         
     }
     assert kb.query_inheritance_relation("Diogo", "eats") == output
+    
 
-def test_test():
+def test_test(data2):
+    
+    kb = data2
+
     output = {
         "Diogo": (["chips"], 0),
-        "person": (["food", "beans"], 1),
-        "mammal": (["banana"], 2),
         
     }
     assert kb.query_inheritance_relation("Diogo", "eats") == output
